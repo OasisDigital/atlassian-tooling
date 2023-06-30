65cff0bf0cac2b417b2a327697d86f75fbd9efaf
Zachary Kipping <zachary.kipping@oasisdigital.com>
2023-06-30 14:27:58 -0500

    Added proper tags to existing nx projects

--- apps/aait-web-app/src/main.ts
+++ apps/aait-web-app/src/main.ts
@@ -1,13 +1,14 @@
-import {
-  AaitFeatureShellComponent,
-  aaitFeatureShellRoutes,
-} from '@aait/aait/feature-shell';
-import { provideAp } from '@aait/util-ap-injector';
 import { bootstrapApplication } from '@angular/platform-browser';
 import {
   provideRouter,
   withEnabledBlockingInitialNavigation,
 } from '@angular/router';
+
+import {
+  AaitFeatureShellComponent,
+  aaitFeatureShellRoutes,
+} from '@aait/aait/feature-shell';
+import { provideAp } from '@aait/util-ap-injector';
 
 bootstrapApplication(AaitFeatureShellComponent, {
   providers: [
